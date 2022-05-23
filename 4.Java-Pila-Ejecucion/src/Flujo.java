e530183700ddceec6256082954f34693251fbe86
Henry Salazar <hjsmipuc@gmail.com>
2022-05-23 12:21:58 -0500

    Manejo de excepciones con try|catch y objetos de errores de java como nullpointer y arithmetic

--- 4.Java-Pila-Ejecucion/src/Flujo.java
+++ 4.Java-Pila-Ejecucion/src/Flujo.java
@@ -4,7 +4,7 @@ public class Flujo {
 		System.out.println("Inicio del método main");
 		metodo1();
 		System.out.println("Fin del método main");
-
+		
 	}
 	
 	private static void metodo1() {
@@ -17,6 +17,30 @@ public class Flujo {
 		System.out.println("Inicio del método2");
 		for (int i = 1; i <= 5; i++) {
 			System.out.println(i);
+	
+			/**
+			 * Manejo de Excepciones
+			 * Try: Intenta hacer...
+			 * Catch: Atrapa el error..
+			 * 
+			 * */
+			
+			try {
+				if (i == 3) {
+					int num = 0;
+					int resultado = i / num;
+					System.out.println(resultado);
+				}
+				
+				String test = null;
+				System.out.println(test.toString());
+				
+			} catch(ArithmeticException | NullPointerException exception) {
+				System.out.println(exception.getMessage());
+				exception.printStackTrace();
+			}
+			
+			
 		}
 		System.out.println("Fin del método2");
 	}
--- excepciones.md
+++ excepciones.md
@@ -4,3 +4,10 @@ Qué es, para qué sirve y cómo funciona la pila de ejecución.
 Qué es la depuración (debug) y para qué sirve.
 Cómo utilizar Eclipse y su perspectiva de debug.
 Cómo cambiar entre perspectivas de Eclipse.
+
+
+Qué son las excepciones, para qué sirven y por qué se utilizarlas.
+Cómo analizar el seguimiento de excepciones o stacktrace.
+Manejar excepciones con bloques try-catch.
+Manejar una excepción lanzada dentro del bloque catch.
+Manejar múltiples excepciones con más de un bloque catcho usando Multi-Catch usando el pipe (|).
