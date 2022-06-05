e9ab8415ccffcde4ca856cf3ea1c779a21b1d8ee
Henry Salazar <hjsmipuc@gmail.com>
2022-06-05 13:52:08 -0500

    Lectura de archivos con java.io curso de numero 7 de java

---
+++ java-io/src/br/com/java/io/test/TestLectura.java
@@ -0,0 +1,26 @@
+package br.com.java.io.test;
+
+import java.io.*;
+
+public class TestLectura {
+	public static void main(String[] args) throws IOException  {
+		
+		//Obtenemos el archivo
+		FileInputStream fileInputString = new FileInputStream("texto.txt");
+		//Procesamos el archivo
+		InputStreamReader inputStreamReader = new InputStreamReader(fileInputString);
+		//Leemos el archivo
+		BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+		
+		String linea;
+		
+		while( (linea = bufferedReader.readLine()) != null ) {
+			System.out.println(linea);
+		}
+		
+		bufferedReader.close();
+		
+	}
+}
+
+
