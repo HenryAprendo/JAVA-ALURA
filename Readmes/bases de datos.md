40f5a9dcd88d50bc0c2c1af00e8977259c50ce45
Henry Salazar <hjsmipuc@gmail.com>
2022-06-17 11:59:50 -0500

    Implementación de un pool de conexiones

--- Readmes/bases de datos.md
+++ Readmes/bases de datos.md
@@ -7,3 +7,10 @@ El JDBC define una capa de abstracción entre la aplicación y el driver de la b
 Esta capa es compuesta de interfaces que el driver implementa.
 Para abrir una conexión con la base de datos debemos utilizar el método getConnection de la clase DriverManager;
 El método getConnection recibe tres parámetros. Son ellos la URL de conexión JDBC, el usuario y la contraseña.
+
+
+# Utilizar el pool de conexiones es una buena práctica;
+El pool de conexiones controla la cantidad de conexiones abiertas entre la aplicación y la base de datos;
+Es normal que haya un mínimo y un máximo de conexiones.
+De la misma forma que hay, en JDBC, una interfaz para representar la conexión (java.sql.Connection), también hay una interfaz que representa el pool de conexiones (javax.sql.DataSource);
+C3P0 es una implementación Java de un pool de conexiones.
--- stop-de-producto/src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
+++ stop-de-producto/src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
@@ -1,15 +1,28 @@
 package com.alura.jdbc.factory;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class ConnectionFactory {
 	
+	private DataSource datasource;
+	
+	public ConnectionFactory() {
+		var pooledDataSource = new ComboPooledDataSource(); 	// Es la clase del driver c3p0, para el pool de conexiones
+		pooledDataSource.setJdbcUrl("jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC");
+		pooledDataSource.setUser("root");
+		pooledDataSource.setPassword("root1234");
+		pooledDataSource.setMaxPoolSize(10);
+		
+		datasource = pooledDataSource;
+	}
+	
 	public Connection recuperaConexion() throws SQLException {
-		return DriverManager.getConnection("jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC",
-				"root", 
-				"root1234");
+		return this.datasource.getConnection();
 	}
 	
 }
